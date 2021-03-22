38e94e3fd2af055d0c3dcbabdcb37cb9e82aaaa9
guicavalheiro <cavalheiro.dev@gmail.com>
2021-03-22 18:33:26 -0300

    real output

--- aula_1/script.js
+++ aula_1/script.js
@@ -1,4 +1,9 @@
-function getDolar() {
-    var dolar = document.getElementById("dolarInput").value
-    alert(dolar)
+var dolar = ''
+var real = ''
+
+function toReal() {
+    dolar = parseFloat(document.getElementById("dolarInput").value) * 5.50
+    //alert(parseFloat(dolar) * 5.50)
+    //alert(document.getElementById("realOutput").innerHTML)
+    document.getElementById("realOutput").innerHTML = 'Conversão: ' + dolar + " R$"
 }
